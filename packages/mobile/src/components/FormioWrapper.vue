7d3385b5ac15cef898d7a0945e7b14e5ab92c142
pmigueld <paolo@acn.fr>
2026-07-24 17:39:17 +0800

    chore(deps): migrate @formio/vue to 6

--- packages/mobile/src/components/FormioWrapper.vue
+++ packages/mobile/src/components/FormioWrapper.vue
@@ -2,7 +2,7 @@
 // Static import is safe here because all consumers (NewView, EditView)
 // are lazy-loaded routes that resolve after registerCustomComponents() in initApp().
 // Do NOT import FormioWrapper eagerly from a non-lazy module.
-import { Form as FormIO } from '@formio/vue/lib/index'
+import { Form as FormIO } from '@formio/vue'
 
 defineOptions({ inheritAttrs: false })
 </script>
